3e1f302175d4296d86067f7bd31d406467131829
Mathias Schaefer (molily) <molily@mailbox.org>
2019-08-30 10:11:35 +0200

    new fonts who dis

--- assets/fonts/dl-noto-sans-fonts.sh
+++ assets/fonts/dl-noto-sans-fonts.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-wget -O noto-sans-normal-bold-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v6/PIbvSEyHEdL91QLOQRnZ14joYw3YTyktCCer_ilOlhE.woff2
-wget -O noto-sans-normal-bold-latin.woff2 https://fonts.gstatic.com/s/notosans/v6/PIbvSEyHEdL91QLOQRnZ1xampu5_7CjHW5spxoeN3Vs.woff2
-wget -O noto-sans-italic-normal-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v6/ByLA_FLEa-16SpQuTcQn4KE8kM4xWR1_1bYURRojRGc.woff2
-wget -O noto-sans-italic-normal-latin.woff2 https://fonts.gstatic.com/s/notosans/v6/ByLA_FLEa-16SpQuTcQn4Igp9Q8gbYrhqGlRav_IXfk.woff2
-wget -O noto-sans-italic-bold-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v6/9Z3uUWMRR7crzm1TjRicDogd9OEPUCN3AdYW0e8tat4.woff2
-wget -O noto-sans-italic-bold-latin.woff2 https://fonts.gstatic.com/s/notosans/v6/9Z3uUWMRR7crzm1TjRicDv79_ZuUxCigM2DespTnFaw.woff2
-wget -O noto-sans-normal-normal-latin.woff2 https://fonts.gstatic.com/s/notosans/v7/LeFlHvsZjXu2c3ZRgBq9nJBw1xU1rKptJj_0jans920.woff2
-wget -O noto-sans-normal-normal-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v7/erE3KsIWUumgD1j_Ca-V-4X0hVgzZQUfRDuZrPvH3D8.woff2
-wget -O noto-sans-italic-normal-latin.woff2 https://fonts.gstatic.com/s/notosans/v7/ByLA_FLEa-16SpQuTcQn4Igp9Q8gbYrhqGlRav_IXfk.woff2
-wget -O noto-sans-italic-normal-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v7/ByLA_FLEa-16SpQuTcQn4KE8kM4xWR1_1bYURRojRGc.woff2
-wget -O noto-sans-italic-bold-latin.woff2 https://fonts.gstatic.com/s/notosans/v7/9Z3uUWMRR7crzm1TjRicDv79_ZuUxCigM2DespTnFaw.woff2
-wget -O noto-sans-italic-bold-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v7/9Z3uUWMRR7crzm1TjRicDogd9OEPUCN3AdYW0e8tat4.woff2
-wget -O noto-sans-normal-bold-latin.woff2 https://fonts.gstatic.com/s/notosans/v7/PIbvSEyHEdL91QLOQRnZ1xampu5_7CjHW5spxoeN3Vs.woff2
-wget -O noto-sans-normal-bold-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v7/PIbvSEyHEdL91QLOQRnZ14joYw3YTyktCCer_ilOlhE.woff2
+
+# Fonts from:
+# https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&display=swap
+
+wget -O noto-sans-normal-normal-latin.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0IIpQlx3QUlC5A4PNr5TRASf6M7Q.woff2
+wget -O noto-sans-normal-normal-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0IIpQlx3QUlC5A4PNr6zRASf6M7VBj.woff2
+
+wget -O noto-sans-normal-bold-latin.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0NIpQlx3QUlC5A4PNjXhFVZNyBx2pqPA.woff2
+wget -O noto-sans-normal-bold-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0NIpQlx3QUlC5A4PNjXhFVatyBx2pqPIif.woff2
+
+wget -O noto-sans-italic-normal-latin.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0OIpQlx3QUlC5A4PNr4ARCQ_mu72Bi.woff2
+wget -O noto-sans-italic-normal-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0OIpQlx3QUlC5A4PNr4ARMQ_mu72BiBLE.woff2
+
+wget -O noto-sans-italic-bold-latin.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0TIpQlx3QUlC5A4PNr4Az5ZuyDzW1IPrie.woff2
+wget -O noto-sans-italic-bold-latin-ext.woff2 https://fonts.gstatic.com/s/notosans/v9/o-0TIpQlx3QUlC5A4PNr4Az5ZuyNzW1IPriezag.woff2
