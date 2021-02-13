fad68f12c554230aa03c788c4ebeb820e1fe1dcc
Mathias Schaefer (molily) <molily@mailbox.org>
2021-02-13 21:45:40 +0100

    Fix TOC entry highlighting

--- assets/highlight-toc.js
+++ assets/highlight-toc.js
@@ -11,42 +11,31 @@
   /** @type {IntersectionObserver} */
   var intersectionObserver;
 
-  var arrayFrom =
-    Array.from ||
-    function (list) {
-      var array = [];
-      for (var i = 0, l = list.length; i < l; i++) {
-        array[i] = list[i];
-      }
-      return array;
-    };
+  /** @type {Set<HTMLHeadingElement>} */
+  var intersectingHeadings = new Set();
 
   function headingIntersect(entries) {
     if (!matchMedia('screen and (min-width: 55rem)').matches) {
       return;
     }
-    var intersectingEntries = entries.filter(function (candidate) {
-      return candidate.isIntersecting;
-    });
-    var relevantIntersectingEntries = intersectingEntries.filter(function (
-      candidate
-    ) {
-      return candidate.target.id !== 'toc-heading';
-    });
-    var firstIntersectingEntry = relevantIntersectingEntries[0];
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        intersectingHeadings.add(entry.target);
+      } else {
+        intersectingHeadings.delete(entry.target);
+      }
+    })
+    var firstIntersectingHeading = intersectingHeadings.values().next().value;
     var firstEntry = entries[0];
     var intersectingHeading;
-    if (
-      firstIntersectingEntry &&
-      firstIntersectingEntry.target !== intersectingHeading
-    ) {
-      intersectingHeading = firstIntersectingEntry.target;
+    if (firstIntersectingHeading) {
+      intersectingHeading = firstIntersectingHeading;
     } else if (
       firstEntry.boundingClientRect &&
       firstEntry.boundingClientRect.y > 0
     ) {
       /* Scrolling upwards: Focus previous heading */
-      var index = arrayFrom(headings).findIndex(function (candidate) {
+      var index = Array.from(headings).findIndex(function (candidate) {
         return candidate === firstEntry.target;
       });
       if (index > 0) {
@@ -58,7 +47,11 @@
         currentTocLink.classList.remove('active');
       }
       var id = intersectingHeading.id;
-      var link = tocTree.querySelector('[href="#' + id + '"]');
+      var tocLinks = Array.from(tocTree.querySelectorAll('a'));
+      var link = tocLinks.find(function(linkCandidate) {
+        var match =  linkCandidate.href.endsWith('/' + id + '/') || linkCandidate.hash === '#' + id;
+        return match
+      });
       if (link) {
         link.classList.add('active');
         link.scrollIntoView({ block: 'nearest' });
@@ -75,20 +68,27 @@
       window.matchMedia &&
       document.body.classList &&
       document.body.scrollIntoView &&
+      Array.from &&
+      Array.prototype.find &&
       Array.prototype.findIndex &&
       Array.prototype.filter &&
-      Array.prototype.forEach
+      Array.prototype.forEach &&
+      String.prototype.endsWith &&
+      window.Set
     ) {
       tocTree = document.getElementById('toc-tree');
-      headings = document.querySelectorAll('h2, h3, h4, h5, h6');
+      headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-      intersectionObserver = new IntersectionObserver(headingIntersect, {
-        root: null,
-        rootMargin: '0px 0px -40% 0px',
-        threshold: 0,
-      });
+      intersectionObserver = new IntersectionObserver(
+        headingIntersect,
+        {
+          root: null,
+          rootMargin: '0px 0px -40% 0px',
+          threshold: 0,
+        }
+      );
 
-      arrayFrom(headings)
+      Array.from(headings)
         .filter(function (heading) {
           return heading.id !== 'toc-heading';
         })
