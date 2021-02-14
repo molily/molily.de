7802df4fdb3bd67ecebf6535efc490f553fc5fa7
Mathias Schaefer (molily) <molily@mailbox.org>
2021-02-14 19:40:54 +0100

    backwards compat

--- assets/testing-angular.js
+++ assets/testing-angular.js
@@ -2,62 +2,49 @@
   'use strict';
 
   function installIframeButtons() {
-    var loadIframeButtons = document.querySelectorAll('.load-iframe');
-    Array.from(loadIframeButtons).forEach(function (button) {
-      button.addEventListener('click', function (event) {
+    var loadIframeButtons = document.getElementsByClassName('load-iframe');
+    for (var i = 0, l = loadIframeButtons.length; i < l; i++) {
+      var button = loadIframeButtons[i];
+      button.onclick = function (event) {
         var button = event.target;
         var scriptTemplate = button.parentNode.nextElementSibling;
         var iframeHTML = scriptTemplate.textContent;
         var container = document.createElement('div');
         container.innerHTML = iframeHTML;
-        scriptTemplate.replaceWith(container);
-        button.remove();
-      });
-    });
+        scriptTemplate.parentNode.replaceChild(container, scriptTemplate);
+        button.parentNode.removeChild(button);
+      };
+    }
   }
 
   function installCollapseTOC() {
     var tocTree = document.getElementById('toc-tree');
     var chapterLinks = tocTree.querySelectorAll(':scope > li > a');
-    Array.from(chapterLinks).forEach(function (chapterLink) {
+    for (var i = 0, l = chapterLinks.length; i < l; i++) {
+      var chapterLink = chapterLinks[i];
       if (chapterLink.pathname !== location.pathname) {
         var ol = chapterLink.nextElementSibling;
         if (ol) {
-          ol.classList.add('collapsed');
+          ol.className += ' collapsed';
         }
       }
-    });
+    }
   }
 
   function install() {
     if (
-      document.createElement &&
-      document.querySelectorAll &&
-      document.addEventListener &&
+      document.getElementsByClassName &&
       'nextElementSibling' in document.body &&
-      document.body.replaceWith &&
-      document.body.remove &&
-      Array.from &&
-      Array.prototype.forEach
+      document.createElement &&
+      document.body.replaceChild &&
+      document.body.removeChild
     ) {
       installIframeButtons();
-    } else if (
-      document.createElement &&
-      document.createTextNode &&
-      document.head
-    ) {
-      var style = document.createElement('style');
-      style.appendChild(document.createTextNode('.load-iframe { display: none }'));
-      document.head.appendChild(style);
     }
     if (
       document.getElementById &&
       document.body.querySelectorAll &&
-      document.body.classList &&
-      document.body.classList.add &&
-      'nextElementSibling' in document.body &&
-      Array.from &&
-      Array.prototype.forEach
+      'nextElementSibling' in document.body
     ) {
       installCollapseTOC();
     }
