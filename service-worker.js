fb4ba4508ada62156352d128e51171b108edca6c
Mathias Schaefer (molily) <molily@mailbox.org>
2021-03-04 15:10:13 +0100

    Service worker: Add offline page

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,14 @@
-const VERSION = 'v3';
+const VERSION = 'v4';
 const PAGES_CACHE = `${VERSION}_PAGES`;
+const OFFLINE_PAGE = 'offline.html';
 const CACHED_RESOURCES = [
+  OFFLINE_PAGE,
   '/',
   '/css/blog.css',
   '/css/book.css',
+  '/assets/manifest.json',
   '/assets/fonts/noto-sans-normal-normal-latin.woff2',
+  '/assets/fonts/noto-sans-italic-normal-latin.woff2',
   '/assets/fonts/noto-sans-normal-bold-latin.woff2',
   '/robust-javascript/',
   '/testing-angular/',
@@ -46,11 +50,19 @@ const CACHED_RESOURCES = [
   '/assets/link-types.js',
   '/assets/highlight-toc.js',
   '/assets/iframe-buttons.js',
-  '/assets/collapse-toc.js'
+  '/assets/collapse-toc.js',
 ];
 
 addEventListener('install', (event) => {
   skipWaiting();
+
+  event.waitUntil(
+    fetch(OFFLINE_PAGE).then((response) =>
+      caches.open(PAGES_CACHE).then((cache) =>
+        cache.put(OFFLINE_PAGE, response)
+      )
+    )
+  );
 });
 
 addEventListener('activate', (event) => {
@@ -60,7 +72,7 @@ addEventListener('activate', (event) => {
         keys
           .filter((key) => !key.startsWith(VERSION))
           .map((key) => {
-            console.log('delete from cache', key);
+            console.log('Delete from cache', key);
             return caches.delete(key);
           })
       )
@@ -97,6 +109,10 @@ addEventListener('fetch', (event) => {
           if (responseFromCache) {
             return responseFromCache;
           }
+          // Serve offline page
+          return caches
+            .open(PAGES_CACHE)
+            .then((cache) => cache.match(OFFLINE_PAGE));
         })
       )
   );
